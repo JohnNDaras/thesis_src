03ee131b835c7b860d7272656a00e9056ffe72ba
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-03-22 09:53:03 +0200

    Create utilities.py

---
+++ Heuristics_Algorithm/utilities.py
@@ -0,0 +1,36 @@
+import csv
+from shapely.geometry import shape
+from shapely.wkt import loads
+import sys
+
+maxInt = sys.maxsize
+while True:
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt/10)
+
+class CsvReader:
+    def readAllEntities(delimiter, inputFilePath):
+        loadedEntities = []
+        geoCollections = 0
+        lineCount = 0  # Counter for lines read
+
+        with open(inputFilePath, newline='') as f:
+            reader = csv.reader(f, delimiter=delimiter)
+            for row in reader:
+                try:
+                    geometry, *information = [s.split(delimiter)[0] for s in row]
+                    geometry = shape(loads(geometry))
+                except:
+                    print("failed")
+                    continue
+                if geometry.geom_type == "GeometryCollection":
+                    geoCollections += 1
+                else:
+                    #print(geometry)
+                    #print(type(geometry))
+                    loadedEntities.append(geometry)
+
+        return loadedEntities
